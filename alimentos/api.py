3f58bdb0fd273a9acef571adb445e5e7e87e7dc8
Ésley Nathan <31036923+Esleynathan@users.noreply.github.com>
2023-05-17 13:47:19 -0300

    REST API'S com Django NINJA | 04 - Path parâmeters #02

--- alimentos/api.py
+++ alimentos/api.py
@@ -1,8 +1,17 @@
 from ninja import Router
 from django.http import JsonResponse
+from typing import List,Dict
 
 alimentos_router = Router()
 
-@alimentos_router.get('/{int:alimento_id}/')
-def get_alimento(request, alimento_id: int):
-    return JsonResponse({'id': alimento_id})
+
+alimentos = [
+    {'Nome': 'Banana', 'Quantidade': 5, 'id': 1},
+    {'Nome': 'Carne', 'Quantidade': 15, 'id': 2},
+    {'Nome': 'Maçã', 'Quantidade': 2, 'id': 3},
+]
+
+@alimentos_router.get('/{int:alimento_id}/', response=List[Dict] )
+def get_alimento(request, alimento_id: int) -> List[Dict]:
+    alimento = list(filter(lambda x: x['id'] == alimento_id, alimentos))
+    return alimento
