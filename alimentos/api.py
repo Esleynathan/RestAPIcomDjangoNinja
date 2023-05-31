502068cd6182ed84597a4dc50472b76b76da8071
Esley <esleynathan@hotmail.com>
2023-05-31 14:48:23 -0300

    REST API'S com Django NINJA | 13 - O objeto HttpResponse

--- alimentos/api.py
+++ alimentos/api.py
@@ -1,7 +1,8 @@
 from ninja import Router
-from .schemas import Alimento
-from .models import Alimento as ModelAlimento
-from django.shortcuts import get_object_or_404
+# from .schemas import Alimento
+# from .models import Alimento as ModelAlimento
+# from django.shortcuts import get_object_or_404
+from django.http import HttpRequest, HttpResponse
 
 alimentos_router = Router()
 
@@ -10,7 +11,12 @@ alimentos_router = Router()
 #     alimento = ModelAlimento.objects.get( id = alimento_id)
 #     return alimento
 
-@alimentos_router.post('/')
-def get_alimento(request, alimento: Alimento):
-    print(alimento.quantidade)
-    return {'quantidade': alimento.quantidade}
+# @alimentos_router.post('/')
+# def get_alimento(request, alimento: Alimento):
+#     print(alimento.quantidade)
+#     return {'quantidade': alimento.quantidade}
+
+@alimentos_router.get('/')
+def get_alimento(request, response: HttpResponse):
+    response.set_cookie('teste_aula', '1234')
+    return{'quantidade': 1}
