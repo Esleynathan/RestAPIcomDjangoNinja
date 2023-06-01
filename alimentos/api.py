c5cf63527a1a28f9f30d2635adaed62338e5bacb
Esley <esleynathan@hotmail.com>
2023-05-31 22:46:58 -0300

    REST API'S com Django NINJA | 15 - Recebendo o token do usuário no backend

--- alimentos/api.py
+++ alimentos/api.py
@@ -3,8 +3,7 @@ from ninja import Router
 # from .models import Alimento as ModelAlimento
 # from django.shortcuts import get_object_or_404
 from django.http import HttpRequest, HttpResponse
-
-alimentos_router = Router()
+from time import sleep
 
 # @alimentos_router.get('/{alimento_id}/', response = Alimento)
 # def get_alimento(request, alimento_id: int) -> Alimento:  
@@ -16,7 +15,9 @@ alimentos_router = Router()
 #     print(alimento.quantidade)
 #     return {'quantidade': alimento.quantidade}
 
+alimentos_router = Router()
+
 @alimentos_router.get('/')
 def get_alimento(request, response: HttpResponse):
-    response.set_cookie('teste_aula', '1234')
-    return{'quantidade': 1}
+
+    return{'quantidade': 1, 'request': request.auth}
--- aula_ninja/api.py
+++ aula_ninja/api.py
@@ -4,13 +4,20 @@ from pacientes.api import pacientes_router
 from ninja.parser import Parser
 import yaml
 from django.http import HttpRequest
+from ninja.security import HttpBearer
 
-class YamlParser(Parser):
-    def parse_body(self, request: HttpRequest):
-        return yaml.safe_load(request.body)
+# class YamlParser(Parser):
+#     def parse_body(self, request: HttpRequest):
+#         return yaml.safe_load(request.body)
 
+# api = NinjaAPI(parser=YamlParser())
 
-api = NinjaAPI(parser=YamlParser())
+class MyAuth(HttpBearer):
+    def authenticate(self, request: HttpRequest, token: str):
+        if token == "1234abc":
+            return token
+
+api = NinjaAPI(auth=MyAuth())
 
 api.add_router('/alimentos', alimentos_router)
 api.add_router('/pacientes', pacientes_router)
