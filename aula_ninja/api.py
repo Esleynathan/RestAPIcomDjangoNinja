3c88e75249588271218e5a321bdd80c0ba9b3a01
Esley <esleynathan@hotmail.com>
2023-06-01 14:21:33 -0300

    REST API'S com Django NINJA | 19 - Basic Auth 02

--- aula_ninja/api.py
+++ aula_ninja/api.py
@@ -2,15 +2,18 @@ from ninja import NinjaAPI
 from alimentos.api import alimentos_router   
 from pacientes.api import pacientes_router 
 from django.http import HttpRequest
+from django.contrib import auth
 
 from ninja.security import HttpBasicAuth
 
 class BasicAuth(HttpBasicAuth):
     def authenticate(self, request, username, password):
-        if username == "admin" and password =="1234":
-            return 1
+        user = auth.authenticate(username=username, password=password)
+
+        if user:
+            return user.id
 
 api = NinjaAPI(auth=BasicAuth())
-
+ 
 api.add_router('/alimentos', alimentos_router)
 api.add_router('/pacientes', pacientes_router)
